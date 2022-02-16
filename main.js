ab31c19bd0945626d0504318c5f13363f10b8bf0
pooja-gera <55778515+pooja-gera@users.noreply.github.com>
2022-02-16 18:33:53 +0530

    FIxes #4
    Fixed animation

--- main.js
+++ main.js
@@ -32,7 +32,7 @@ function scrollActive(){
     sections.forEach(current =>{
         const sectionHeight = current.offsetHeight
         const sectionTop = current.offsetTop - 50;
-        sectionId = current.getAttribute('id')
+        const sectionId = current.getAttribute('id')
 
         if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
             document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
@@ -93,7 +93,7 @@ themeButton.addEventListener('click', () => {
 const sr = ScrollReveal({
     distance: '30px',
     duration: 1800,
-    reset: true,
+    
 });
 
 sr.reveal(`.home__data, .home__img, .team__head, .team-card,
